c5481a0d3887a47f3b8137af3a8d2da452eebd45
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-08-09 22:45:10 +0530

    added folder structure

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -1,11 +1,27 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login"
+import List from "./pages/list/List"
+import Single from "./pages/single/Single"
+import New from "./pages/new/New"
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <h1>hello welcome helloe</h1>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home/>}></Route>
+            <Route path="login" element={<Login/>}></Route>
+          </Route>
+          <Route path="users">
+            <Route index element={<List/>}/>
+            <Route path=":userId" element={<Single/>}></Route>
+            <Route path="new" element={<New/>}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
