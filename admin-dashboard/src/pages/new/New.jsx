6dac19e10991d4b61037dfbd2e348f1fac969473
Mohammed Ajsal <ajsal12aju@gmail.com>
2023-08-18 19:00:14 +0530

    started newpage

--- admin-dashboard/src/pages/new/New.jsx
+++ admin-dashboard/src/pages/new/New.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import "./new.scss"
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import ProfileImage from "../../images/pro-removebg-preview (1).png";
 
 function New() {
   return (
-    <div>
-      neeewwww
+    <div className='new'>
+      <Sidebar/>
+      <div className="newContainer">
+        <Navbar/>
+       <div className="top">
+        <h1>Add New User</h1>
+       </div>
+       <div className="bottom">
+        <div className="left">
+        <img src={ProfileImage} alt="" />
+        </div>
+        <div className="right">
+          right
+        </div>
+       </div>
+        data table
+      </div>
     </div>
   )
 }
